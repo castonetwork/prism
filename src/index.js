56c9f884b1b2047234ac85506cc89927d5f04b09
hhan <hhan87@gmail.com>
2019-01-23 01:09:23 +0900

    fix: replace lng lat position

--- src/index.js
+++ src/index.js
@@ -15,9 +15,8 @@ const initNode = async () => {
   console.log(">> node is ready", node.peerInfo.id.toB58String());
   // setup a libp2p node
   serviceId = isBrowser ? new URL(location.href).searchParams.get('serviceId') : process.argv[2];
-  longitude = parseFloat(isBrowser ? new URL(location.href).searchParams.get('lng') : process.argv[3]);
-  latitude = parseFloat(isBrowser ? new URL(location.href).searchParams.get('lat') : process.argv[4]);
-
+  latitude = parseFloat(isBrowser ? new URL(location.href).searchParams.get('lat') : process.argv[3]);
+  longitude = parseFloat(isBrowser ? new URL(location.href).searchParams.get('lng') : process.argv[4]);
   let coords = !isNaN(latitude) && !isNaN(longitude) && {longitude, latitude} || undefined;
   setupNode({ node, serviceId, coords});
 };
